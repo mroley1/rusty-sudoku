0f8314cd7563bf3a91f422ba035ebbd6b53e6848
mroley1 <64291507+mroley1@users.noreply.github.com>
2024-06-09 19:42:10 -0400

    board printer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+
+fn print_board(board: [[usize; 9]; 9]) {
+    println!("+-------+-------+-------+");
+    let mut i = 0;
+    for row in board {
+        print!("|");
+        let mut j = 0;
+        for value in row {
+            if value == 0 {
+                print!("  ")
+            } else {
+                print!(" {}", value);
+            }
+            if j == 2 || j == 5 || j == 8 {
+                print!(" |")
+            }
+            j = j + 1;
+        }
+        println!("");
+        if i == 2 || i == 5 || i == 8 {
+            println!("+-------+-------+-------+")
+        }
+        i = i + 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board: [[usize; 9]; 9] = [[0; 9]; 9];
+    print_board(board);
 }
